298e20de10e0ec5652e557875d8e83fd563266b2
Michelle C. <nmich.cor@gmail.com>
2026-04-09 13:39:19 +0200

    Revert "feat[AUTH-05]: add register form with validation and API error handling"

--- frontend/src/services/auth.services.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const register = async (data: { email: string; password: string }) => {
-    return await axios.post('http://localhost:3000/auth/register', { email: data.email, password: data.password })
-}
