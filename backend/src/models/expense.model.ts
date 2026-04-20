d2ae5324f0a85a3c0de1ee5f7003097aaa53dffd
Michelle Cor <nmich.cor@gmail.com>
2026-04-20 21:44:41 +0200

    feat(XPNS-04): add ExpenseItem type, findListExpenseByMonth model and rename findExpenseByMonth>findTotalExpenseByMonth model

--- backend/src/models/expense.model.ts
+++ backend/src/models/expense.model.ts
@@ -1,11 +1,16 @@
-import type { Expense, CreateExpenseDTO, ExpenseResponse } from '../types/expense.types.js';
+import type {
+    Expense,
+    CreateExpenseDTO,
+    ExpenseResponse,
+    ExpenseItem
+} from '../types/expense.types.js';
 import getPool from './db.js';
 
 export const createExpense = async (data: CreateExpenseDTO): Promise<ExpenseResponse> => {
     const result = await getPool().query<Expense>(
         `INSERT INTO expenses (user_id, category_id, amount, description, date)
-    VALUES ($1, $2, $3, $4, $5)
-    RETURNING id, category_id, amount, description, date`,
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, category_id, amount, description, date`,
         [data.user_id, data.category_id, data.amount, data.description, data.date]
     );
     return result.rows[0] as ExpenseResponse;
@@ -16,15 +21,28 @@ export const findExpenseByUserId = async (user_id: string): Promise<Expense | nu
         `SELECT * FROM expenses WHERE user_id = $1`,
         [user_id]
     );
-
     return result.rows[0] ?? null;
 };
 
-export const findExpenseByMonth = async (user_id: string, month: string): Promise<{ totalExpense: number } | null> => {
+export const findTotalExpenseByMonth = async (user_id: string, month: string): Promise<{ totalExpense: number } | null> => {
     const result = await getPool().query<{ totalExpense: number }>(
-        `SELECT SUM(amount) as totalExpense FROM expenses WHERE user_id = $1 AND TO_CHAR(date, 'YYYY-MM') = $2`,
+        `SELECT SUM(amount) as totalExpense 
+        FROM expenses 
+        WHERE user_id = $1 AND TO_CHAR(date, 'YYYY-MM') = $2`,
         [user_id, month]
     );
-
     return result.rows[0] ?? null;
+};
+
+export const findListExpenseByMonth = async (user_id: string, month: string): Promise<ExpenseItem[]> => {
+    const result = await getPool().query<ExpenseItem>(
+        `SELECT
+            expenses.id, amount, description, date, category_id, name AS category_name
+        FROM expenses
+        INNER JOIN categories ON expenses.category_id = categories.id
+        WHERE user_id = $1 AND TO_CHAR(date, 'YYYY-MM') = $2
+        ORDER BY date DESC`,
+        [user_id, month]
+    );
+    return result.rows;
 };
--- backend/src/services/budget.service.ts
+++ backend/src/services/budget.service.ts
@@ -1,11 +1,11 @@
 import { findBudgetByMonth } from "../models/budget.model.js"
-import { findExpenseByMonth } from "../models/expense.model.js"
+import { findTotalExpenseByMonth } from "../models/expense.model.js"
 import type { BudgetMonth } from "../types/budget.types.js"
 
 export async function getBudgetSummary(user_id: string, month: string): Promise < BudgetMonth | null > {
     const budget = await findBudgetByMonth(user_id, month)
     if (!budget) return null
-    const total = await findExpenseByMonth(user_id, month)
+    const total = await findTotalExpenseByMonth(user_id, month)
     const total_spent = total?.totalExpense as number ?? 0
     const budgetAmount = budget.amount as number
     const remaining =  budgetAmount - total_spent
