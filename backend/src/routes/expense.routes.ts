1c972ff836011082f5023b5e03ebfd737792266d
Michelle Cor <nmich.cor@gmail.com>
2026-04-21 19:46:07 +0200

    feat(XPNS-03): add GET /api/v1/expenses/summary/:month route

--- backend/src/routes/expense.routes.ts
+++ backend/src/routes/expense.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getExpensesByMonth, registerExpense } from "../controller/expense.controller.js";
+import { getExpensesByCategories, getExpensesByMonth, registerExpense } from "../controller/expense.controller.js";
 
 const router = Router();
 
 router.post('/expenses', registerExpense);
+router.get('/expenses/summary/:month', getExpensesByCategories);
 router.get('/expenses/:month', getExpensesByMonth);
 
 export default router;
